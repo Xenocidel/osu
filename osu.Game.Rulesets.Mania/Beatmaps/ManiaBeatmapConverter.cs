5d8e7c9c0c80fef4a0420f77e87ed411041e9bf8
smoogipooo <smoogipooo@gmail.com>
2017-05-12 15:51:09 +0900

    Add basic specific-beatmap conversion.

--- osu.Game.Rulesets.Mania/Beatmaps/ManiaBeatmapConverter.cs
+++ osu.Game.Rulesets.Mania/Beatmaps/ManiaBeatmapConverter.cs
@@ -1,22 +1,37 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using osu.Game.Beatmaps;
-using osu.Game.Rulesets.Mania.Objects;
-using System.Collections.Generic;
-using System;
-using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Beatmaps;
+using osu.Game.Rulesets.Mania.Objects;
+using System;
+using System.Collections.Generic;
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
+using System.Linq;
 
 namespace osu.Game.Rulesets.Mania.Beatmaps
 {
-    internal class ManiaBeatmapConverter : BeatmapConverter<ManiaHitObject>
+    public class ManiaBeatmapConverter : BeatmapConverter<ManiaHitObject>
     {
         protected override IEnumerable<Type> ValidConversionTypes { get; } = new[] { typeof(IHasXPosition) };
 
+        protected override Beatmap<ManiaHitObject> ConvertBeatmap(Beatmap original)
+        {
+            // Todo: This should be cased when we get better conversion methods
+            var converter = new LegacyConverter(original);
+
+            return new Beatmap<ManiaHitObject>
+            {
+                BeatmapInfo = original.BeatmapInfo,
+                TimingInfo = original.TimingInfo,
+                HitObjects = original.HitObjects.SelectMany(converter.Convert).ToList()
+            };
+        }
+
         protected override IEnumerable<ManiaHitObject> ConvertHitObject(HitObject original, Beatmap beatmap)
         {
+            // Handled by the LegacyConvereter
             yield return null;
         }
     }
